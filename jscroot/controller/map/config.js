d0b55269711019fd8dcbe17c8b8c126716063b19
Bimo231 <arga.bimo.09@gmail.com>
2024-02-12 12:18:28 +0700

    first commit

---
+++ jscroot/controller/map/config.js
@@ -0,0 +1,45 @@
+import { fromLonLat } from 'https://cdn.skypack.dev/ol/proj.js';
+import { container } from 'https://jscroot.github.io/element/croot.js';
+
+const attributions = '<a href="https://petapedia.github.io/" target="_blank">&copy; PetaPedia Indonesia</a> ';
+
+const place = [107.5451098,
+    -6.8739993];
+
+const basemap = new ol.layer.Tile({
+    source: new ol.source.OSM({
+        attributions: attributions,
+    }),
+});
+
+const defaultstartmap = new ol.View({
+    center: fromLonLat(place),
+    zoom: 13.2,
+});
+
+export const overlay = new ol.Overlay({
+    element: container('popup'),
+    autoPan: {
+        animation: {
+        duration: 250,
+        },
+    },
+  });
+
+export const popupinfo = new ol.Overlay({
+    element: container('popupinfo'),
+    autoPan: {
+        animation: {
+        duration: 250,
+        },
+    },
+});
+
+export let map = new ol.Map({
+    layers: [
+        basemap
+    ],
+    overlays: [overlay,popupinfo],
+    target: 'map',
+    view: defaultstartmap,
+});
---
+++ jscroot/controller/map/template.js
@@ -0,0 +1,16 @@
+export let xxx = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/bimo-getjson";
+export let aaa = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/bimo-postpoint";
+export let bbb = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/bimo-postlinestring";
+export let ccc = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/bimo-postpolygon";
+export let qqq = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/geointersect";
+export let www = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/bimoWithin";
+export let eee = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/bimoNear";
+export let rrr = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/bimoNearsphere";
+export let ttt = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/bimoBox";
+export let yyy = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/bimoCenter";
+export let uuu = "/postcentersphere";
+
+export let clickpopup = `
+Coordinate  : #COORDINATE#<br>
+Type  : #TYPE#<br>
+`
